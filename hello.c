2cc2f400cad76ae2f10927a951013c4e9e7d7dac
Danielle <dastacy@cisco.com>
2020-10-12 17:05:01 -0400

    added favorite color feature

--- hello.c
+++ hello.c
@@ -3,6 +3,7 @@
 int main(void)
 {
 	char name[100];
+	char color[100];
 	int age;
 
 	printf("What's your name? ");
@@ -11,7 +12,10 @@ int main(void)
 	printf("How old are you? ");
 	scanf("%d", &age);
 
-	printf("Hello, %s! You are %d years old\n", name, age);
+	printf("What's your favorite color? ");
+	scanf("%s", color);
+
+	printf("Hello, %s! You are %d years old, and your favorite color is %s\n", name, age, color);
 
 	return 0;
 }
